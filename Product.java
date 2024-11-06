64be4e5bb621abeab9b1543661876cbda41c7e44
ramvakkapatla <ramanjaneyuluvakkapatla@gmail.com>
2024-11-06 22:28:32 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,14 @@
+class Product
+{
+public static void main(String args[])
+{
+String prdname="galaxy m32";
+int prdid=20153;
+String prdcategory="mobiles";
+double prdprice=15000.25;
+System.out.println("product name:"+prdname);
+System.out.println("product id:"+prdid);
+System.out.println("product category:"+prdcategory);
+System.out.println("product price:"+prdprice);
+}
+}
