29f3e197f65be1e0149602d202d813d5db1ee640
ramvakkapatla <ramanjaneyuluvakkapatla@gmail.com>
2024-11-11 22:47:07 +0530

    Add files via upload

---
+++ DAY1/Student.java
@@ -0,0 +1,14 @@
+class Student
+{
+public static void main(String args[])
+{
+int stdroll=101;
+String stdname="NAIDU";
+String stdcourse="MCA";
+double stdfee=50000.25;
+System.out.println("student roll no is:"+stdroll);
+System.out.println("student name is:"+stdname);
+System.out.println("student course is:"+stdcourse);
+System.out.println("student fee is:"+stdfee);
+}
+}
